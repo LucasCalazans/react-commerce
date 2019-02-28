3f397ad0a24c8427bd3843f9371aab2f5c31ce1b
Pedro Esteves <pedro.esteves@webjump.com.br>
2019-02-28 14:48:17 -0300

    36 - Added media query definition for use with styled-components.

---
+++ src/helpers/styles/mediaQuery.js
@@ -0,0 +1,25 @@
+import { css } from 'styled-components';
+
+const sizes = {
+    largeDesktop: 1400,
+    desktop: 1280,
+    tabletLandscape: 1024,
+    tablet: 768,
+    phoneLandscape: 480,
+    phone: 360,
+};
+
+// iterate through the sizes and create a media template
+const media = Object.keys(sizes).reduce((accumulator, label) => {
+    // use em in breakpoints to work properly cross-browser and support users
+    // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
+    const emSize = sizes[label] / 16;
+    accumulator[label] = (...args) => css`
+        @media (min-width: ${emSize}em) {
+            ${css(...args)};
+        }
+    `;
+    return accumulator;
+}, {});
+
+export default media;
