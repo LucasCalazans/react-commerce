4617fe38a49db50b3cae36c953f45bfb05dc57d1
Nicholas Kamimura <nickkami@hotmail.com>
2019-03-11 17:54:33 -0300

    Remove Link from index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 module.hot && module.hot.accept();
 
