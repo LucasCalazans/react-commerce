6ec5b9c94300157ed2217149e756fafaa29e55b6
Nicholas Kamimura <nickkami@hotmail.com>
2019-04-02 11:20:25 -0300

    Commit Hover on buttons

--- src/components/Button/__stories__/button.jsx
+++ src/components/Button/__stories__/button.jsx
@@ -5,9 +5,5 @@ import Button from '../index';
 const stories = storiesOf('Button', module);
 
 stories.add('Button', () => {
-    return (
-        <Button color="primary" size="small">
-            Hello World
-        </Button>
-    );
+    return <Button color="tertiary">Hello World</Button>;
 });
