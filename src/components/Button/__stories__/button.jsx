3b623f50495071b46572bbdd2032d4636d0ccf75
nick <nickkami@hotmail.com>
2019-05-08 21:23:27 -0300

    Solved conflicts

--- src/components/Button/__stories__/button.jsx
+++ src/components/Button/__stories__/button.jsx
@@ -4,7 +4,6 @@ import Button from '../index';
 
 const stories = storiesOf('Button', module);
 
-<<<<<<< HEAD
 stories.add('Button', () => {
     return (
         <>
@@ -25,29 +24,3 @@ stories.add('Button', () => {
         </>
     );
 });
-=======
-const getVersions = color => (
-    <Fragment>
-        <Button color={color}>{color}</Button>
-        <br />
-        <Button color={color} size="small">
-            Small
-        </Button>
-        <br />
-        <Button color={color} size="large">
-            Large
-        </Button>
-        <br />
-        <br />
-    </Fragment>
-);
-
-stories.add('Button', () => (
-    <Fragment>
-        {getVersions('primary')}
-        {getVersions('secondary')}
-        {getVersions('danger')}
-        {getVersions('warning')}
-    </Fragment>
-));
->>>>>>> master
